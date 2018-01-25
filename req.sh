4b72bb2605456e10daaa05922176e739e71c3957
root <root@localhost.localdomain>
2018-01-25 23:02:16 +0100

    fix request null issue

---
+++ req.sh
@@ -0,0 +1,5 @@
+curl -v -H "Content-Type: application/json" -H "Accept: application/json" -X POST -d '{"id":0,"username":"test01","password":"Alma1234","description":"This is a test user"}' http://localhost:8080/login
+
+curl -v -H "Content-Type: application/json" -H "Accept: application/json" -X POST -d '{"id":1,"username":"test02","password":"Alma7896","description":"This is a test user"}' http://localhost:8080/register
+
+
